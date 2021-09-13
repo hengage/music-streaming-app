63efef4660044690e78692a3ea1039ef23d062a5
Yusuf Samaila <yusuf7samaila@gmail.com>
2021-09-13 11:42:56 +0100

    Create react app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Music streaming App Frontend
+## React + Bootstrap CSS
+
+
+### Running the App locally
+
+- Clone the repo by running a `git clone <url>` in your command terminal.
+- cd into the cloned repo by running a `cd <repo name>`.
+- Install all dependencies using `yarn install`.
+- Start the development server by running `yarn start`.
+
+**Happy Contributing**
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css/"
+
+function App() {
+  return (
+    <div className="container text-center">
+      <header className="App-header">
+        <hr/>
+        <h3>Music streaming app</h3>
+        <p>
+          Built on React and bootstrap. yarn is the package manager of choice here!
+        </p>
+        <p>Happy Contributing...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
