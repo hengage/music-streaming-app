6129004fd547702e7f933feb7473644ad7e1362d
hengage <tunecarterkillz@gmail.com>
2021-10-26 04:04:10 +0100

    created Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css/"
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <div className="container text-center">
       <header className="App-header">
-        <hr/>
-        <h3>Music streaming app</h3>
-        <p>
-          Built on React and bootstrap. yarn is the package manager of choice here!
-        </p>
-        <p>Happy Contributing...</p>
+        <Sidebar />
       </header>
     </div>
   );
---
+++ src/components/sidebar.js
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+    return ( 
+        <section className="sidebar">
+            <div className="container">
+                <img 
+                src="../images/peermusic_logo.png" 
+                alt="peermusic logo"
+                 className="logo" /
+                 >
+            </div>
+
+        </section>
+     );
+}
+ 
+
+export default Sidebar;
