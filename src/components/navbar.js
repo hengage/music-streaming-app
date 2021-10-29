d4c1bea9d82743dfe78f7782afd4b9867c0c055f
hengage <tunecarterkillz@gmail.com>
2021-10-29 01:37:29 +0100

    fixed menubar toggle class

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,21 @@
+import { useState, useEffect} from "react";
 import "../css/navbar.css";
-
-const toggleMenu = (x) => {
-    x.classList.toggle("change");
-  }
-
 
 
 
 const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => { 
+        setIsActive(!isActive);
+        console.log(setIsActive)
+        console.log(isActive)
+    };
+
+    useEffect(() => {
+        
+    })
+
     return ( 
         <section className="navbar">
             <div className="container">
@@ -20,11 +28,18 @@ const Navbar = () => {
                 <div className="nav">
                     <div 
                         className="menu-btn" 
-                        onClick={()=>toggleMenu(this)}
+                        id="menubtn"
+                        onClick={toggleMenu}
                     >
-                        <div className="bar1"></div>
-                        <div className="bar2"></div>
-                        <div className="bar3"></div>
+                        <div className={`bar1 ${isActive ? "change": "bar1"}`}></div>
+                        <div className={`bar2 ${isActive ? "change": ""}`}></div>
+                        <div className={`bar3 ${isActive ? "change": ""}`}></div>
+
+                        {/* <div className={isActive ? "bar1" : "hidden"}></div>
+                        <div className={isActive ? "bar2" : "hidden"}></div>
+                        <div className={isActive ? "bar3" : "hidden"}></div> */}
+
+                        
                     </div>
 
                     <div className="nav-item">
